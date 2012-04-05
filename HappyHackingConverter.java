bd9db542be811a4250fd4944f9e4441d53930f0f
Vladimir <IsThisAddressPublic@mailinator.com>
2012-04-05 15:31:41 -0400

    Basic structure of project classes.

--- HappyHackingConverter.java
+++ HappyHackingConverter.java
@@ -3,30 +3,134 @@ import javax.swing.JFrame;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
-
+import javax.swing.*;
+import java.awt.*;
 import javax.swing.JOptionPane;
 
 import java.awt.*;
 import java.awt.event.*;
 
 
+
+/**
+ * A very simple toy application with an intuitive UI.
+ * We start with a simple class which will contain main,
+ * create and launch the gui and serve as a container 
+ * for the other classes specific to this project.
+ * And lets use easily remembered variables names like:
+ */
 class HappyHackingConverter
 {
+	/**
+	 * I would describe the default widget colors used as soul numbing gray.
+	 * So instead we are going to use this variable as the background color throughout our toy app.
+	 */
+	private final static Color white = new Color(255,255,255);
+	
+	/**
+	 * All classes in this toy app will be contained in this class.
+	 * As they are specific to this projet and should have access to shared variables like the background color.
+	 * And if we do decide to share them with other projects we can always refactor them out.
+	 */
+
+	/*
+	 * The first inner class we will use will be a slightly customized JPanel.
+	 * We are also going to start using the leading C (as in Convert) convention in class nameing. 
+	 */
+	 class CPanel extends JPanel
+	 {
+		public CPanel()
+		{
+			super();
+			setBackground(white);
+		}
+	 }//end of class CPanel
+	 
+	/**
+     * We are gonig to put our cutomized panels in a custom split pane. 
+     */
+	class CSplit extends JSplitPane
+	{
+		public CSplit(int newOrientation, boolean newContinuousLayout, Component newLeftComponent, Component newRightComponent) 
+		{
+			super(newOrientation, newContinuousLayout, newLeftComponent, newRightComponent);
+		}
+	}//end of CSPlit class.
+
+	/**
+     * And lastly the frame class to contain everything.
+     */
+	class CFrame extends JFrame 
+	{
+		public CFrame() 
+		{
+			super("Converter between Imperial and Metric.");
+			
+			setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+			
+			JMenu menu = new JMenu("Application");
+			menu.setBackground(white);
+			menu.setMnemonic(KeyEvent.VK_A);
+			
+			//quit item
+			JMenuItem item = new JMenuItem("Quit");
+			item.setBackground(white);
+			item.setMnemonic(KeyEvent.VK_Q);
+			item.addActionListener(new ActionListener() 
+			{
+				public void actionPerformed(ActionEvent e) 
+				{
+					System.out.println("Quit request");
+					System.gc();
+					System.exit(0);
+				}
+			}
+			);
+			menu.add(item);
+
+			JMenuBar menuBar = new JMenuBar();
+			menuBar.setBackground(white);
+			menuBar.add(menu);
+			setJMenuBar(menuBar);
+			
+			setSize(new Dimension(200, 200));
+			
+			setVisible(true);
+		}//end of CFrame constructor
+		
+	}//end of CFrame class.
+	
+	//Constructor
 	public HappyHackingConverter()
 	{
+		//Make sure things compile.
 		System.out.println("Hello World!");
-	}
+		
+		CFrame f = new CFrame();
+		f.setBackground(white);
+		
+		JPanel a = new CPanel();
+		JPanel b = new CPanel();
+		
+		
+		CSplit split = new CSplit(JSplitPane.HORIZONTAL_SPLIT, true, a, b);
+		
+		f.getContentPane().add(split);
+	}//end of HappyHackingConverter constructor
 
     /**
      * Create the GUI and show it.  For thread safety,
      * this method should be invoked from the
      * event-dispatching thread.
      */
-    private static void createAndShowGUI() {
+    private static void createAndShowGUI() 
+	{
         HappyHackingConverter conv = new HappyHackingConverter();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+	{
+		System.gc();
         //Schedule a job for the event-dispatching thread:
         //creating and showing this application's GUI.
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
@@ -34,6 +138,6 @@ class HappyHackingConverter
                 createAndShowGUI();
             }
         });
-    }
-
-}
+    }//end of main
+	
+}//end of HappyHackingConverter
