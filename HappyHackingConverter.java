4dfaf7eb46114f8109e76592ab9f9b8b0610e7ae
Vladimir <IsThisAddressPublic@mailinator.com>
2012-04-11 00:27:18 -0400

    Using a document filter to prevent user from entering invalid values.

--- HappyHackingConverter.java
+++ HappyHackingConverter.java
@@ -4,12 +4,15 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import javax.swing.*;
+import javax.swing.text.*;
 import java.awt.*;
 import javax.swing.JOptionPane;
 
 import java.awt.*;
 import java.awt.event.*;
-
+import java.awt.Toolkit;
+import java.lang.Double;
+import java.lang.StringBuffer;
 
 
 /**
@@ -33,17 +36,101 @@ class HappyHackingConverter
 	 * And if we do decide to share them with other projects we can always refactor them out.
 	 */
 
+	/**
+	 * We will want to prevent input of invalid values.
+	 * A validator will allow us to correct invalid input when the widget is about to lose focus.
+	 * How ever what we want is to prevent the appearence of invalid input at all.
+	 * This is why we need a customized document filter.
+	 * And with this class we are  going to start using the leading C (as in Convert) convention in class naming. 
+	 */
+	 class CDocumentTemperatureFilter extends DocumentFilter
+	 {		
+		public CDocumentTemperatureFilter()
+		{
+			System.out.println("Constructed CDocumentTemperatureFilter");
+		}
+		
+		//This is supposed to check if the resulting text would be a valid temperature. 
+		//For simplicity's sake that just means any valid double. 
+		//We declare to throw bad location due to doc.getText, but that should never happen.... in theory.
+		//If insert is true we insert, if false we replace. 
+		//Length parameter only used when replacing.
+		private void checkIfTextIsValidTemperature(boolean insert, FilterBypass fb, int offs,
+                             String str, int length, AttributeSet a) throws BadLocationException
+				{
+					try
+					{
+						Document doc = fb.getDocument();
+						String text = doc.getText(0, doc.getLength());
+						StringBuffer sb = new StringBuffer(text);
+						if(insert) 
+						{ 
+							sb.insert(offs, str); 
+							Double.parseDouble(sb.toString()); //This does not allow starting with - (minus), fix it!
+							super.insertString(fb, offs, str, a);
+						}
+						else
+						{
+							sb.replace(offs, offs+length, str);
+							Double.parseDouble(sb.toString()); //Code duplication!
+							super.replace(fb, offs, length, str, a);
+						}
+						
+					}//try
+					catch(NumberFormatException ex)
+					{
+						Toolkit.getDefaultToolkit().beep();
+					}
+				}//End of check if valid temperature
+		
+		public void insertString(FilterBypass fb, int offs,
+                             String str, AttributeSet a)
+        throws BadLocationException 
+		{
+			System.out.println("in DocumentSizeFilter's insertString method");
+			checkIfTextIsValidTemperature(true, fb, offs, str, 0, a);
+		}//insertString
+    
+		public void replace(FilterBypass fb, int offs,
+							int length, 
+							String str, AttributeSet a)
+			throws BadLocationException 
+		{
+			System.out.println("in DocumentSizeFilter's replace method");
+			checkIfTextIsValidTemperature(false, fb, offs, str, length, a);
+		}//replace
+	 }//End of temperature filter. 
+	 
 	/*
-	 * The first inner class we will use will be a slightly customized JPanel.
+	 * This inner class we will use will be a slightly customized JPanel.
 	 * We are also going to start using the leading C (as in Convert) convention in class nameing. 
 	 */
 	 class CPanel extends JPanel
 	 {
 		public CPanel()
 		{
-			super();
+			super(new GridBagLayout());//call to super must be first statement in constructor
+			GridBagLayout gridbag = (GridBagLayout)getLayout();
+			
 			setBackground(white);
-		}
+			
+			JTextPane textPane = new JTextPane();
+			StyledDocument styledDoc = textPane.getStyledDocument();
+			if (styledDoc instanceof AbstractDocument) 
+			{
+				AbstractDocument doc = (AbstractDocument)styledDoc;
+				doc.setDocumentFilter(new CDocumentTemperatureFilter());
+			} 
+			else 
+			{
+				System.err.println("Text pane's document isn't an AbstractDocument!");
+				System.exit(-1);
+			}
+			
+			GridBagConstraints c = new GridBagConstraints();
+			gridbag.setConstraints(textPane, c);
+			add(textPane);
+		}//constructor
 	 }//end of class CPanel
 	 
 	/**
